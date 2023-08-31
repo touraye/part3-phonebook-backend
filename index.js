968cd21c91710e3acacd02de7763f3a1c42cca66
touraye <touraye07@gmail.com>
2023-08-31 23:04:00 +0000

    phonebook full stack

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const app = express()
 const cors = require('cors')
 
 app.use( express.json() )
-app.use(cors())
+app.use( cors() )
+app.use(express.static('dist'))
 app.use(
 	morgan(':method :url :status :res[content-length] - :response-time ms :body')
 )
