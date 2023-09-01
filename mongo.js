4b1d4d4e9b548973c7b5a9dafd555cd5fa3bdc2e
touraye <touraye07@gmail.com>
2023-09-01 16:40:22 +0000

    Command-line database

---
+++ mongo.js
@@ -0,0 +1,43 @@
+const mongoose = require( 'mongoose' )
+
+if (process.argv.length < 3) {
+	console.log('give password as argument')
+	process.exit(1)
+}
+
+const password = process.argv[ 2 ]
+const name = process.argv[ 3 ]
+const number = process.argv[ 4 ]
+
+const url = `mongodb+srv://ebrima:${password}@sandbox.ooueg.mongodb.net/phonebook?retryWrites=true&w=majority`
+const URL = `mongodb://localhost:27017//phonebook`
+
+mongoose.set('strictQuery', false)
+mongoose.connect( url )
+
+const personSchema = new mongoose.Schema( {
+    name: String,
+    number: String
+} )
+
+const Person = mongoose.model( 'Person', personSchema )
+
+const person = new Person( {
+    name,
+    number
+})
+
+
+person.save().then( result => {
+    console.log(`Added ${name} ${number} to phonebook`)
+    
+    mongoose.connection.close()
+} )
+
+
+Person.find( {} ).then( res => {
+    console.log('Phonebook:');
+    res.forEach( person => console.log( person.name, ' ', person.number ) )
+    
+    mongoose.connection.close()
+})
