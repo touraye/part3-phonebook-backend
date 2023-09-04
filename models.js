52b3e298a6d086c817e14e288c97e07a5f61788e
touraye <touraye07@gmail.com>
2023-09-04 10:20:22 +0000

    Deploying the database backend to production

--- models.js
+++ models.js
@@ -2,7 +2,7 @@ const mongoose = require( 'mongoose' )
 
 mongoose.set( 'strictQuery', false )
 
-const url = process.env.MongoDB_URI
+const url = process.env.MONGODB_URI
 
 console.log('Connecting to ', url);
 
